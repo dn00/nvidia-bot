8c2e981273dd9231f1f2a56554e88a7ea4423b26
dn00 <dtnguy90@gmail.com>
2020-09-26 19:29:31 -0700

    Added options

--- stores/newegg_checker.py
+++ stores/newegg_checker.py
@@ -3,6 +3,7 @@ from chromedriver_py import binary_path  # this will get you the path variable
 from utils.logger import log
 import webbrowser
 import time
+import random
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -13,9 +14,15 @@ from selenium.webdriver.common.by import By
 
 options = Options()
 options.page_load_strategy = "eager"
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option("useAutomationExtension", False)
-prefs = {"profile.managed_default_content_settings.images": 2}
+options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36')
+# options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option("useAutomationExtension", False)
+# prefs = {"profile.managed_default_content_settings.images": 2}
+
+DEFAULT_HEADERS = {
+    "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+}
 
 # CHECK_URL = "https://www.newegg.com/p/pl?N=100007709%20601357282%20601321572"
 CHECK_URL = "https://www.newegg.com/p/pl?N=100007709%20601357282"
@@ -23,8 +30,9 @@ CHECK_URL = "https://www.newegg.com/p/pl?N=100007709%20601357282"
 ATC_URL = "https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList="
 CART_URL = "https://secure.newegg.com/Shopping/ShoppingCart.aspx?Submit=view"
 ATC_TEXT_LOWERCASE = "add to cart"
+AYH_TEXT_LOWERCASE = "are you human?"
 
-List = {'N82E16814126453', 'N82E16814487518', 'N82E16814137597', 'N82E16814126452', 'N82E16814487519', 'N82E16814487521'}
+List = {'N82E16814126453', 'N82E16814487518', 'N82E16814137597', 'N82E16814126454', 'N82E16814487526', 'N82E16814126452', 'N82E16814487519', 'N82E16814487521'}
 
 class NeweggChecker:
     def __init__(self, headless, delay):
@@ -32,12 +40,19 @@ class NeweggChecker:
             enable_headless()
         self.driver = webdriver.Chrome(executable_path=binary_path, options=options)
         self.delay = delay
+        self.basePageTitle = ""
 
     def check(self):
         self.driver.get(CHECK_URL)
         EC.presence_of_element_located((By.CLASS_NAME, "page-footer"))
+        self.basePageTitle = self.driver.title
 
         while True:
+            log.info(self.driver.title)
+            while self.driver.title != self.basePageTitle:
+                log.info(self.driver.title)
+                self.wait_enter_captcha()
+
             item_containors = self.driver.find_elements(By.CLASS_NAME, "item-container")
             log.info(f'{len(item_containors)} items found. Checking stock...')
             productIds = []
@@ -56,9 +71,14 @@ class NeweggChecker:
                 time.sleep(0.5)
                 webbrowser.open(f'{CART_URL}')
                 return
-            time.sleep(self.delay)
+
+            time.sleep(random.randint(self.delay, self.delay + 4))
             self.driver.refresh()
 
+
+    def wait_enter_captcha(self):
+        time.sleep(self.delay)
+        log.info("Please enter captcha")
 
     def is_atc(self, item):
         try:
